34fc6f4620c851399a1be73e4b4ca9011f964827
SimonPeric <s.peric@alumno.edu.ar>
2023-04-10 17:39:56 -0300

    Añadido. No funciona. Test incluidos

---
+++ diccionarios.py
@@ -0,0 +1,18 @@
+import unittest
+diccionario = {"hola":1,"mundo":1}
+def contarpalabras(text):
+    diccionario.values
+
+
+
+
+class TestDecimalToRoman(unittest.TestCase):
+    
+    def test_1(self):
+        
+        resultado=contarpalabras("hola")
+        
+        self.assertEqual(resultado,{"Hola":1})
+
+if __name__ == "__main__":
+    unittest.main()
